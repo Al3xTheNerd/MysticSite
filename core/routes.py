b818c1c606518ae6427b5a22f5eda6318f0d5c0f
Joshua Deason <joshua.deason@zoho.com>
2025-02-19 21:15:50 -0700

    correct stuff

--- core/routes.py
+++ core/routes.py
@@ -146,10 +146,10 @@ import git
 @app.route('/webhook', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        repo = git.Repo('./myproject')
-        origin = repo.remotes.origin
-        repo.create_head('master', 
-    origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
+        repo = git.Repo('./MysticItem')
+        origin = repo.remotes.main
+        repo.create_head('main', 
+    origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
         origin.pull()
         return '', 200
     else:
