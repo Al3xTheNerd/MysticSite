37d83cdc362baeba74de9a481d9d621732ea9a7a
Joshua Deason <joshua.deason@zoho.com>
2025-02-19 23:05:27 -0700

    Add a check to make it easier to test the site on local machine alongside development

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,12 @@
 from core import app
 from flask_sqlalchemy import SQLAlchemy
-
+from sys import platform
 app.static_url_path="core/static/"
 app.secret_key = b'\xb9Xv\xcf\xf5\x9eB\xfc\xc1\x1b\x8cg\xd3"$f.\xdf9\x8b\xeb{\x956/c\xdb\xe2\x9dS\xcb\x92\x14\xae\xcf\xb7~2I\xf6Y\xb9\r\xed\xcb\x99N )x'
 
-#if __name__ == "__main__":
-#    app.run()
+
+if __name__ == "__main__" and platform == "win32":
+    app.run()
 from core import db
 with app.app_context():
     db.create_all()
