ec1eae08ab479f03dff7f0a4eac613c7ca9af4fd
Joshua Deason <joshua.deason@zoho.com>
2025-02-25 16:43:12 -0700

    Update secret key and hide it to prep for repo being public

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,9 @@
 from core import app
 from flask_sqlalchemy import SQLAlchemy
 from sys import platform
+from c import secret_key
 app.static_url_path="core/static/"
-app.secret_key = b'\xb9Xv\xcf\xf5\x9eB\xfc\xc1\x1b\x8cg\xd3"$f.\xdf9\x8b\xeb{\x956/c\xdb\xe2\x9dS\xcb\x92\x14\xae\xcf\xb7~2I\xf6Y\xb9\r\xed\xcb\x99N )x'
+app.secret_key = secret_key
 
 
 if __name__ == "__main__" and platform == "win32":
