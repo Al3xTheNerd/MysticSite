ec499c1e81f105d0a46afb634afd33c8fa4fc87c
Joshua Deason <joshua.deason@zoho.com>
2025-07-07 19:37:00 -0600

    undo mysql changes, too much bullshit ;(

--- core/__init__.py
+++ core/__init__.py
@@ -2,20 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 from core import config as c
-from atn import secret_key, dbConn
+from atn import secret_key
 from flask_login import LoginManager
-from sys import platform
 
 
 app = Flask(__name__, instance_path = os.path.abspath("core/"))
 
 app.config.from_object(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-if platform == "win32":
-    app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///database.db'
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = dbConn
-    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle' : 280}
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///database.db'
 app.static_url_path="core/static/"
 app.secret_key = secret_key
 
