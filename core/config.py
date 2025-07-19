218f11be758bde5c0ffd879cd64f6770880e7fb0
Joshua Deason <joshua.deason@zoho.com>
2025-07-19 17:36:26 -0600

    Adjust Glazed Terra Image Locations

--- core/config.py
+++ core/config.py
@@ -71,24 +71,7 @@ for tag in nonCatTags:
     if tag not in validTags:
         validTags.append(tag)
 
-Colors = [
-    "black", 
-    "blue", 
-    "brown", 
-    "cyan", 
-    "gray", 
-    "green",
-    "light_blue",
-    "light_gray",
-    "lime",
-    "magenta",
-    "orange",
-    "pink",
-    "purple",
-    "red",
-    "white",
-    "yellow"
-    ]
+
 BackgroundImages = [
     ('black_glazed_terracotta.webp', 'blue_glazed_terracotta.webp'),
     ('black_glazed_terracotta.webp', 'brown_glazed_terracotta.webp'),
