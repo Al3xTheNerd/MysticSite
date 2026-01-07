70b559929196c25691050544f68db369cd061e87
Joshua Deason <joshua.deason@zoho.com>
2026-01-07 13:39:58 -0700

    When an admin user clicks n an item it takes you to the manage item screen now. Useful for bulk updates.

--- core/config.py
+++ core/config.py
@@ -290,7 +290,8 @@ Changelog = {
         ("mc-green", "Crate list is now it's own page. Sorted by both type and release date."),
         ("mc-green", "Tag list is now it's own page. Sorted by type."),
         ("mc-green", "New tags added! <code>Treefeller, Veinminer, Axe Projectile, Radius Breaking, Block Counter, Bleed, Kill Counter, Telekinesis, Speed Attribute, Custom Loot, and Furniture Dropping</code>"),
-        ("mc-green", "Full item list is now paginated, this should help for those of you with shitty internet.")
+        ("mc-green", "Full item list is now paginated, this should help for those of you with shitty internet."),
+        ("mc-light-purple", "Admin users now go straight to manage item screen when clicking an item preview.")
     ]
 }
 
