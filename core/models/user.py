48d9a9a4415c0d9cedbc53aa56edc5ee84dfd7bf
Joshua Deason <joshua.deason@zoho.com>
2026-01-28 14:49:38 -0700

    Expand character limit on User: Password

--- core/models/user.py
+++ core/models/user.py
@@ -4,6 +4,6 @@ from core import db
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    username = db.Column(db.String(1000))
+    password = db.Column(db.String(1000))
+    username = db.Column(db.String(100))
     permissions = db.Column(db.Integer)
