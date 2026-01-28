4842433d1b733a9db51226d03c6e9c53291e24ca
Joshua Deason <joshua.deason@zoho.com>
2026-01-28 14:49:16 -0700

    Add preliminary Postgresql Database Connection

--- core/__init__.py
+++ core/__init__.py
@@ -1,23 +1,34 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from sshtunnel import SSHTunnelForwarder
 
 from atn import *
 from flask_login import LoginManager
 
 
 app = Flask(__name__, instance_path = os.path.abspath("core/"))
-
-app.config.from_object(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///database.db'
 app.static_url_path="core/static/"
 app.secret_key = secret_key
 
+forwarding_server = SSHTunnelForwarder(
+    (ssh_host, ssh_port),
+    ssh_username=ssh_user,
+    ssh_password=ssh_pass,
+    remote_bind_address=(db_host, db_port)
+)
+forwarding_server.start()
+local_bind_port = forwarding_server.local_bind_port
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_pass}@127.0.0.1:{local_bind_port}/MysticMC'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///database.db'
 db = SQLAlchemy(app)
 from core.models import *
 with app.app_context():
     db.create_all()
+
+
+
 
 login_manager = LoginManager(app=app)
 login_manager.login_view = '/login' # type: ignore
