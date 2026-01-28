87e3eab2c2d9af4993d4d195b389f3ebe4149645
Joshua Deason <joshua.deason@zoho.com>
2026-01-28 16:20:37 -0700

    maybe add support for server side connection

--- core/__init__.py
+++ core/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 from sshtunnel import SSHTunnelForwarder
+from sys import platform
 
 from atn import *
 from flask_login import LoginManager
@@ -11,15 +12,18 @@ app = Flask(__name__, instance_path = os.path.abspath("core/"))
 app.static_url_path="core/static/"
 app.secret_key = secret_key
 
-forwarding_server = SSHTunnelForwarder(
-    (ssh_host, ssh_port),
-    ssh_username=ssh_user,
-    ssh_password=ssh_pass,
-    remote_bind_address=(db_host, db_port)
-)
-forwarding_server.start()
-local_bind_port = forwarding_server.local_bind_port
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_pass}@127.0.0.1:{local_bind_port}/MysticMC'
+if platform == "win32":
+    forwarding_server = SSHTunnelForwarder(
+        (ssh_host, ssh_port),
+        ssh_username=ssh_user,
+        ssh_password=ssh_pass,
+        remote_bind_address=(db_host, db_port)
+    )
+    forwarding_server.start()
+    local_bind_port = forwarding_server.local_bind_port
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_pass}@127.0.0.1:{local_bind_port}/MysticMC'
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/MysticMC'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///database.db'
 db = SQLAlchemy(app)
